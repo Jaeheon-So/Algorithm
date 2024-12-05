464cf17583e40deacaee89719ca3dbdad4b0a977
Jaeheon So <79908684+Jaeheon-So@users.noreply.github.com>
2024-12-05 23:21:42 +0900

    Delete 프로그래머스/1 directory

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++
@@ -1,6 +0,0 @@
-function solution(nums) {
-    let len = nums.length / 2
-    let set = new Set(nums)
-    
-    return len <= set.size ? len : set.size
-}
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++
@@ -1,6 +0,0 @@
-function solution(participant, completion) {
-    participant.sort()
-    completion.sort()
-    
-    return participant.find((p, idx) => p !== completion[idx])
-}
